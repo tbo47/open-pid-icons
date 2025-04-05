76ec58dfe4e78b5bed3937fb83f99254c0023a20
tbo47 <thibaut.lassalle@gmail.com>
2025-04-05 08:39:55 +0000

    change format

--- generate-svgs.js
+++ generate-svgs.js
@@ -13,7 +13,7 @@ if (!fs.existsSync(outputDir)) {
 
 // Generate SVG files for each valve
 let readmeContent = `## Generated SVG Icons\n\n`;
-jsonData.valve.forEach((valve) => {
+jsonData.data.forEach((valve) => {
     const svgContent = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="80" height="80">
     <rect width="100" height="100" fill="white" />
